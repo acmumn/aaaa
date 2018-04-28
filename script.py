05684c932f5671d7e602c96969de7c3ecde245d0
Michael Zhang <failed.down@gmail.com>
2018-04-27 23:10:21 -0500

    kill newlines

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ async def on_message(message):
 async def on_message_delete(message):
     if message.author == client.user:
         return
-    content_sanitized = message.clean_content
+    content_sanitized = message.clean_content.replace("\n", "\\n")
     author = message.author.nick or message.author.name
     await client.send_message(message.channel, "[deleted] {}: {}".format(author, content_sanitized))
 
@@ -29,8 +29,8 @@ async def on_message_edit(before, after):
     if before.author == client.user or before.clean_content == after.clean_content:
         return
     author = before.author.nick or before.author.name
-    before_sanitized = before.clean_content
-    after_sanitized = after.clean_content
+    before_sanitized = before.clean_content.replace("\n", "\\n")
+    after_sanitized = after.clean_content.replace("\n", "\\n")
     await client.send_message(before.channel, "[edited] {}:\nfrom: {}\nto: {}".format(author, before_sanitized, after_sanitized))
 
 client.run(config.token)
