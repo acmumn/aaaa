0a4e9909251ce3194c9948ff2d259f796fa5e9ad
Michael Zhang <failed.down@gmail.com>
2018-04-27 23:37:17 -0500

    sigh

--- script.py
+++ script.py
@@ -4,6 +4,15 @@ import config
 
 client = discord.Client()
 
+def username(user):
+    res = user.name
+    if user.nick:
+        res = "{} ({})".format(user.nick, user.name)
+    return res
+
+def sanitize(msg):
+    return msg.replace("\n", "\\n").replace("`", " \` ")
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -20,17 +29,17 @@ async def on_message(message):
 async def on_message_delete(message):
     if message.author == client.user:
         return
-    content_sanitized = message.clean_content.replace("\n", "\\n").replace("`", " \` ")
-    author = message.author.nick or message.author.name
+    content_sanitized = sanitize(message.clean_content)
+    author = username(message.author)
     await client.send_message(message.channel, "[deleted] {}: {}".format(author, content_sanitized))
 
 @client.event
 async def on_message_edit(before, after):
     if before.author == client.user or before.clean_content == after.clean_content:
         return
-    author = before.author.nick or before.author.name
-    before_sanitized = before.clean_content.replace("\n", "\\n").replace("`", " \` ")
-    after_sanitized = after.clean_content.replace("\n", "\\n").replace("`", " \` ")
+    author = username(before.author)
+    before_sanitized = sanitize(before.clean_content)
+    after_sanitized = sanitize(after.clean_content)
     await client.send_message(before.channel, "[edited] {}:\nfrom: {}\nto: {}".format(author, before_sanitized, after_sanitized))
 
 client.run(config.token)
