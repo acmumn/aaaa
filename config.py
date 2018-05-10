80c8560b3d63ba0ced91a9fc8bb88518a0354b99
Louis Goessling <louis@goessling.com>
2018-05-09 21:03:42 -0500

    remove commented out prints... lol

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ import json
 token = os.getenv("BOT_TOKEN")
 log_channels = json.loads(os.getenv("BOT_CHANNELS", "{}"))
 
-# print("Bot Token: ", token)
-# print("Channels: ",  log_channels)
-
 if not token:
     sys.stderr.write("No token specified. Please set the BOT_TOKEN environment variable.")
     sys.exit(1)
