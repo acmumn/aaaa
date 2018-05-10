e5467bc43593647ffb3b11d13469f2326a5a50b3
Michael Zhang <failed.down@gmail.com>
2018-05-09 20:43:27 -0500

    so let's not have print statements in our config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import json
 token = os.getenv("BOT_TOKEN")
 log_channels = json.loads(os.getenv("BOT_CHANNELS", "{}"))
 
-print("Bot Token: ", token)
-print("Channels: ",  log_channels)
+# print("Bot Token: ", token)
+# print("Channels: ",  log_channels)
 
 if not token:
     sys.stderr.write("No token specified. Please set the BOT_TOKEN environment variable.")
